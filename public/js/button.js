3bee368f6e69335782fb811bc2d7fc4a448a7cfa
Seedi Madisha <seedimadisha@gmail.com>
2024-08-13 01:03:26 +0200

    Adding changes to the project

---
+++ public/js/button.js
@@ -0,0 +1,8 @@
+function messageOwner() {
+    // Send a message to the owner using your preferred method (e.g., email, SMS, messaging platform)
+    // For example, using a hypothetical messaging API:
+    messagingAPI.sendMessage({
+      recipient: +27769743248,
+      message: 'Booking confirmed! Details: [insert details]'
+    });
+  }
